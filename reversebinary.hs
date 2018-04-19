3f0e4006c11f05cd686b84cd3d5b2e34a938c783
Jonathan Goodman <goodmanjonathan@sbcglobal.net>
2018-04-18 21:42:23 -0500

    simplify reversebinary

--- reversebinary.hs
+++ reversebinary.hs
@@ -1,20 +1,22 @@
-import Data.List
+import Data.Char
+import Text.Printf
 
-toRevBinaryDigits' :: Int -> [Int]
-toRevBinaryDigits' 0 = []
-toRevBinaryDigits' n
-    | n `mod` 2 == 0 = 0 : toRevBinaryDigits' (n `div` 2)
-    | n `mod` 2 == 1 = 1 : toRevBinaryDigits' (n `div` 2)
+--toRevBinaryDigits' :: Int -> [Int]
+--toRevBinaryDigits' 0 = []
+--toRevBinaryDigits' n
+--    | n `mod` 2 == 0 = 0 : toRevBinaryDigits' (n `div` 2)
+--    | n `mod` 2 == 1 = 1 : toRevBinaryDigits' (n `div` 2)
 
-toRevBinaryDigits :: Int -> [Int]
-toRevBinaryDigits 0 = [0]
-toRevBinaryDigits n = toRevBinaryDigits' n
+--toRevBinaryDigits :: Int -> [Int]
+--toRevBinaryDigits 0 = [0]
+--toRevBinaryDigits n = toRevBinaryDigits' n
 
-toDecimal :: [Int] -> Int
-toDecimal (x:[]) = x
-toDecimal (x:xs) = x * 2^length xs + toDecimal xs
+binaryToDecimal :: String -> Int
+binaryToDecimal s = foldl (\acc (c, idx) ->
+        acc + digitToInt c * 2^idx
+    ) 0 (zip s [(length s - 1), (length s - 2)..0])
 
 main :: IO ()
 main = do
-    n <- readLn
-    print $ toDecimal (toRevBinaryDigits n)
+    n <- readLn :: IO Int
+    print $ binaryToDecimal (reverse $ printf "%b" n)
